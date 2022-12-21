f407673f79609fb142ca976a778ed83cf1d52693
Marco <posta.email3001@gmail.com>
2022-12-21 10:58:23 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  name: "App",
+  components: {
+    AppHeader,
+  },
+
+}
+
+</script>
+
+<template>
+  <AppHeader />
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<script>
+import { store } from '../store';
+
+export default {
+  name: "AppHeader",
+  data() {
+    return {
+      store,
+    }
+  }
+}
+
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
+
