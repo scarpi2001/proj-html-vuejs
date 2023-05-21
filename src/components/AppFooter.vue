cc8feeafc58d3a8dc7a30f262118d8bf3ac26812
sca <posta.email3001@gmail.com>
2023-05-21 14:21:35 +0200

    fix

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,72 +1,76 @@
 <script>
-import AppMenu from './AppMenu.vue'
+import AppMenu from "./AppMenu.vue";
 
 export default {
-    name: "AppFooter",
-    components: {
-        AppMenu,
-    },
-    data() {
-        return {
-            socials: ["fa-brands fa-facebook-f", "fa-brands fa-instagram", "fa-brands fa-twitter", "fa-brands fa-youtube"]
-        }
-    }
-}
-
+  name: "AppFooter",
+  components: {
+    AppMenu,
+  },
+  data() {
+    return {
+      socials: [
+        "fa-brands fa-facebook-f",
+        "fa-brands fa-instagram",
+        "fa-brands fa-twitter",
+        "fa-brands fa-youtube",
+      ],
+    };
+  },
+};
 </script>
 
 <template>
-    <!-- footer -->
-    <footer>
-        <div class="container">
-            <!--  logo -->
-            <img src="../assets/images/logo_seo_1x.png" alt="">
+  <!-- footer -->
+  <footer>
+    <div class="container">
+      <!--  logo -->
+      <img src="/images/logo_seo_1x.png" alt="" />
 
-            <!-- menu -->
-            <nav>
-                <AppMenu />
-            </nav>
+      <!-- menu -->
+      <nav>
+        <AppMenu />
+      </nav>
 
-            <span>
-                Copyright 2012-2022 | Avada Theme by <b>ThemeFusion</b> | All Rights Reserved | Powered by
-                <b>Scarpi</b>
-            </span>
+      <span>
+        Copyright 2012-2022 | Avada Theme by <b>ThemeFusion</b> | All Rights
+        Reserved | Powered by
+        <b>Scarpi</b>
+      </span>
 
-            <!-- socials -->
-            <div id="socials">
-                <a v-for="(social, index) in socials" :key="index" href="#">
-                    <font-awesome-icon :icon=social />
-                </a>
-            </div>
-        </div>
-    </footer>
-
+      <!-- socials -->
+      <div id="socials">
+        <a v-for="(social, index) in socials" :key="index" href="#">
+          <font-awesome-icon :icon="social" />
+        </a>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <style lang="scss" scoped>
-@use "../styles/partials/variables" as*;
+@use "../styles/partials/variables" as *;
 
 footer {
-    padding: 50px 0;
-    color: $tertiary;
+  padding: 50px 0;
+  color: $tertiary;
 
-    nav {
-        margin: 60px 0;
+  nav {
+    margin: 60px 0;
+  }
+
+  #socials {
+    display: flex;
+    justify-content: center;
+    margin-top: 50px;
+
+    a {
+      font-size: 28px;
+      margin: 0 10px;
     }
+  }
 
-    #socials {
-        display: flex;
-        justify-content: center;
-        margin-top: 50px;
-
-        a {
-            font-size: 28px;
-            margin: 0 10px;
-        }
-    }
-
-    b {
-        color: black;
-    }
+  b {
+    color: black;
+  }
 }
 </style>
